eb31c45621d9cbbe220748ece9cdb6bb486d851f
Klee Collage <klee.collage@gmail.com>
2024-07-15 15:43:16 -0500

    Views and ImageGalery
    NoteView and NothingSelectedView designed, ImageGaleryComponent added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,26 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout >
-      <Typography variant="h5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, qui quod enim quo, odit inventore doloremque cum tenetur corrupti at officiis adipisci?</Typography>
-      {/* Nothing selected */}
-      {/* Note view */}
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
