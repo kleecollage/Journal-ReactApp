b7d9516597d0892b1cbec2d28f923c315d5120f8
Klee Collage <klee.collage@gmail.com>
2024-07-15 14:55:43 -0500

    Layouts in journal
    sidebar and navbar components designed as layouts un /src/jounal/laoyout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
-import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout >
+      <Typography variant="h5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, qui quod enim quo, odit inventore doloremque cum tenetur corrupti at officiis adipisci?</Typography>
+      {/* Nothing selected */}
+      {/* Note view */}
+    </JournalLayout>
   )
 }
