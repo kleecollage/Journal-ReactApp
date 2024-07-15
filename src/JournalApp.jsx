b723b47a7f0f80131333ec1a8a7f590087800791
Klee Collage <klee.collage@gmail.com>
2024-07-14 20:14:28 -0500

    MUI + Vite configuration

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,10 @@
+import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
+
 export const JournalApp = () => {
   return (
-    <h1>JournalApp</h1>
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
   )
 }
